30bdfa4ad7d1c96e9fd7caf566d7c26fb14fb5ab
serraguti <pacoserranox@gmail.com>
2025-06-25 11:25:54 +0200

    up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #DEBEMOS TRAERNOS NUESTRAS LIBRERIAS DE FASTAPI
 #Traemos el método FastApi que nos permite crear nuestra Aplicación
 from fastapi import FastAPI
+from typing import Union
 #Creamos una variable para la aplicación
 app = FastAPI()
 
@@ -9,9 +10,23 @@ app = FastAPI()
 @app.get("/")
 def metodoRoot():
     #Este método puede hacer mil cosas, if, bucles
-    #Si el método es para un Api SIEMPRE debe devolver algo
-    return {"mensaje": "Hoy es lunes y mañana martes"}
+    #Si el método es para un Api
+    return {"mensaje": "Hoy es miercoles"}
 
+@app.get("/saludo")
+def metodoSaludo():
+    return {"mensaje": "Bienvenido a mi FastApi"}
+
+@app.get("/doble/{numero}")
+def getDoble(numero: int):
+    #INTERNAMENTE NO ES NECESARIO EL TIPADO
+    doble: int = numero * 2
+    return {"doble": doble}
+
+@app.get("/saludito")
+def dameSaludito(nombre: str, aficion: Union[str, None]=None):
+    return {"saludo": "Hola holita " + nombre
+            , "aficion": aficion}
 
 
 
