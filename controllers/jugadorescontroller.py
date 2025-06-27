d56732fd85a77ecf0872b3e2d739d269400cc7b1
serraguti <pacoserranox@gmail.com>
2025-06-27 11:03:18 +0200

    filter

--- controllers/jugadorescontroller.py
+++ controllers/jugadorescontroller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 import services.ServiceJugadores as service
 from utils.response_wrapper import api_response
 
@@ -12,4 +12,17 @@ def readPlayers():
 @router.get("/findplayer/{idplayer}")
 def findPlayer(idplayer: int):
     player = service.findPlayer(idplayer)
-    return api_response(data=player)
+    if (player is None):
+        raise HTTPException(status_code=404, detail=f"player no found {idplayer}")
+    else:
+        return api_response(data=player)
+    
+@router.get("/filteredad/{edad}")
+def playersAge(edad: int):
+    players = service.searchPlayersAge(edad)
+    return api_response(data=players)
+
+@router.get("/filterposicion/{posicion}")
+def playersPosition(posicion: str):
+    players = service.searchPlayersPosition(posicion)
+    return api_response(data=players)
--- services/ServiceJugadores.py
+++ services/ServiceJugadores.py
@@ -29,3 +29,16 @@ def findPlayer(idPlayer: int):
             jugador.posicion = row["posicion"]
             jugador.edad = int(row["edad"])
             return jugador
+    return None
+
+#METODOS PARA FILTRAR
+def searchPlayersAge(edad: int):
+    players = getPlayers()
+    filtro = list(filter(lambda x: x.edad > edad, players))
+    return filtro
+
+#METODOS PARA FILTRAR
+def searchPlayersPosition(posicion: str):
+    players = getPlayers()
+    filtro = list(filter(lambda x: x.posicion.lower() == posicion.lower(), players))
+    return filtro
